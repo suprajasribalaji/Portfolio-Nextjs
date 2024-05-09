ac9019ff2d0821d27c626b54eae8da09877d88e7
suprajasribalaji <suprajasri.balaji@gmail.com>
2024-05-10 01:53:45 +0530

    base setup

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Portfolio</title>
       </Head>
-
+      Hello
       <style jsx global>{`
         html,
         body {
