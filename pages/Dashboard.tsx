ca776bce56faa61268b8be07ff1441f167bb8289
suprajasribalaji <suprajasri.balaji@gmail.com>
2024-05-10 02:12:07 +0530

    base setup

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,14 +1,21 @@
 import { NextPage } from "next";
 import styled from "styled-components";
+import { LinearGradientColor } from "../styles/theme";
+import BackgroundImage from "../public/images/home-bg-img.jpeg";
 
 const Dashboard: NextPage = () => {
     return (
         <DashboardPage>
-            Hi
         </DashboardPage>
     )
 };
 
 export default Dashboard;
 
-const DashboardPage = styled.div``;
+const DashboardPage = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(0deg, ${LinearGradientColor.homeLG}, ${LinearGradientColor.homeLG}),
+    url(${BackgroundImage.src});
+  background-size: cover;
+`;
